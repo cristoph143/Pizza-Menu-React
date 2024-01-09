dc0dd5395ce6306fd10982bc9a088b75f9e94eb4
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2024-01-09 16:28:29 +0800

    Revert "Merge branch 'main' into feat/pizza"
    This reverts commit 0354c95873549f618e87307659643d8bab500702, reversing
    changes made to fc2dee0d4cda88c63ec7bf0fc5d700e4fc697ad3.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import pizzas from "./pizzas.json";
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div className="Menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  console.log(hour);
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  // if (hour > openHour && hour < closeHour) {
+  //   alert("We're Currently open");
+  // } else {
+  //   alert("We're currently closed");
+  // }
+  return (
+    <footer className="footer">
+      {new Date().toLocaleDateString}We're currently open!
+    </footer>
+  );
+  // return React.createElement("footer", null, "We're currently open!");
+}
+
+function Pizza() {
+  console.log(pizzas);
+  return (
+    <div>
+      <h2>Pizza</h2>
+      <img src="pizzas/bbq_chicken_pizza.jpg" alt="Pizza BBQ" />
     </div>
   );
 }
